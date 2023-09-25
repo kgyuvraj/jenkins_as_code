5084dbddbd4df608286eb6329faab0c05008bb19
yuvraj <kgyuvraj@gmail.com>
2023-09-25 17:10:40 +0530

    dynamic-job added

--- examples/dynamic-jobs/src/main.groovy
+++ examples/dynamic-jobs/src/main.groovy
@@ -1,1 +1,25 @@
-println("Hello World !!!!!")
+@Grab('org.yaml:snakeyaml:1.17')
+import hudson.model.*
+ArrayList searchYamlFiles(String dirPath) {
+    ArrayList file_list = []
+    File dir = new File(dirPath)
+    if (dir.isDirectory()) {
+        dir.eachFileRecurse { file ->
+            if (file.isFile() && (file.name.endsWith(".yaml") || file.name.endsWith(".yml"))) {
+                file_list.add(file.absolutePath)
+            }
+        }
+    } else {
+        println("Error: $dirPath is not a valid directory.")
+    }
+    return file_list
+}
+
+// get the current working directory
+def cwd = hudson.model.Executor.currentExecutor().getCurrentWorkspace().absolutize()
+pipeline_file_list = searchYamlFiles(cwd.toString())
+
+for (current_pipeline in pipeline_file_list) {
+
+    println("Working on :" + current_pipeline)
+}
