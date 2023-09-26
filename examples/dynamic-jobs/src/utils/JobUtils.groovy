318e533cc48b51d67ae202ae0e5a2295b003b143
yuvraj <kgyuvraj@gmail.com>
2023-09-26 12:27:04 +0530

    dynamic-job added

--- examples/dynamic-jobs/src/utils/JobUtils.groovy
+++ examples/dynamic-jobs/src/utils/JobUtils.groovy
@@ -6,7 +6,8 @@ class JobUtils {
     def job_config
     JobUtils(current_pipeline){
         def parsed_job_config = new Yaml().load((current_pipeline as File).text)
-        this.job_config = job_config
+        this.job_config = parsed_job_config
+//        this.file_path = current_pipeline
     }
     String get_job_name(){
         return this.job_config.job_name
