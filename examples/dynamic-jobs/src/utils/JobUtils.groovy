49e117ae14b2521914d9f18d008a1aa3613779f5
yuvraj <kgyuvraj@gmail.com>
2023-09-26 16:18:54 +0530

    dynamic-job added

--- examples/dynamic-jobs/src/utils/JobUtils.groovy
+++ examples/dynamic-jobs/src/utils/JobUtils.groovy
@@ -12,4 +12,7 @@ class JobUtils {
     String get_job_name(){
         return this.job_config.job_name
     }
+    String get_build_command(){
+        return this.job_config.build_command
+    }
 }
